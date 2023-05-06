ad718279451f24bc0a56c19bda868429af3dbb8e
Edward <edwardonionc020901@gmail.com>
2023-05-07 01:02:15 +0800

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Map, { Layer, Source } from 'react-map-gl';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      mapboxAccessToken='pk.eyJ1IjoiZWR3YXJkb25pb25jIiwiYSI6ImNsYnZ5amRzajAzcXUzbnJ3dXo4aXgzbmEifQ.nwp6x4W6ffop_GeCAtIE2g'
+      initialViewState={{
+        longitude: 114,
+        latitude: 22.4,
+        zoom: 14,
+        pitch:60
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      terrain={{source:'mapbox-dem', exaggeration:1.5}}
+    >
+      <Source id='mapbox-dem' type='raster-dem' url='mapbox://mapbox.mapbox-terrain-dem-v1' tileSize={512}>
+        <Layer id='sky' type='sky' paint={
+          {
+            "sky-type":'atmosphere',
+            'sky-atmosphere-sun': [0.0, 0.0],
+            'sky-atmosphere-sun-intensity': 15
+          }
+        }/>
+      </Source>
+    </Map>
   );
 }
-
-export default App;
